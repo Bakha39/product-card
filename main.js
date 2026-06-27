5af1860cefcf1940e5061dd8d09a94d523288a08
Bakha39 <rbaxruz05@gmail.com>
2026-06-27 21:33:23 +0200

    Переименовал названия переменных и классов

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const toggleBtnCard = document.querySelector('#toggle-color-card')
+const toggleColorCardBtn = document.querySelector('#toggle-color-card')
 const productCard = document.querySelector('.product-card')
 
-toggleBtnCard.addEventListener('click', () => {
+toggleColorCardBtn.addEventListener('click', () => {
     productCard.classList.toggle('is-product-card');
     console.log('Кнопка нажата');
 })
@@ -14,18 +14,18 @@ const colors = [
     'rgba(150, 150, 255, 0.3)'
 ]
 
-const btnCards = document.querySelector('#change-color-cards')
+const changeColorsCardsBtns = document.querySelector('#change-colors-cards')
 const productCards = document.querySelectorAll('.product-card')
 
-btnCards.addEventListener('click', () => {
+changeColorsCardsBtns.addEventListener('click', () => {
     productCards.forEach((cards, index) => {
         cards.style.backgroundColor = colors[index]; 
     })
 })
 
 
-const googlePages = document.querySelectorAll('.product-card__button')
-googlePages.forEach((elements) => {
+const openGooglePages = document.querySelectorAll('.product-card__button')
+openGooglePages.forEach((elements) => {
 elements.addEventListener('click', () => {
     alert('Внимание: вы переходите по внешней ссылке');
     window.open('https://google.com', '_blank');      
@@ -36,7 +36,7 @@ titleName.addEventListener('mouseover', () => {
     console.log(titleName);
 })
 
-const ToggleBtnColor = document.querySelector('#toggle-color-button')
-ToggleBtnColor.addEventListener('click', () => {
-    ToggleBtnColor.classList.toggle('title__button--color3');
+const toggleColorBtn = document.querySelector('#toggle-color-button')
+toggleColorBtn.addEventListener('click', () => {
+    toggleColorBtn.classList.toggle('title__button--color3');
 })
