ad363d2cc04e497a4ec6a1d4cdf416bc007d55da
Bakha39 <rbaxruz05@gmail.com>
2026-07-23 17:09:25 +0200

    laid out the footer and added form

---
+++ homework-11.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // чтобы страница не перезагружалась
+  const fromData = new FormData(form)
+  const newData = Object.fromEntries(fromData)
+  console.log(newData);
+});
+
+//5
+const openBtn = document.querySelector(".button-registration");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
+const modal = document.querySelector(".modal");
+
+openBtn.addEventListener("click", () => {
+  overlay.classList.add("open");
+  modal.classList.add("showed")
+});
+
+closeBtn.addEventListener("click", () => {
+  overlay.classList.remove("open");
+  modal.classList.remove("showed")
+});
+
+
+//
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("password-confirm");
+const registerForm = document.getElementById("registerForm");
+const message = document.getElementById("message")
+
+registerForm.addEventListener("submit", (event) => {
+
+  if (!registerForm.checkValidity()) {
+    event.preventDefault();
+    message.textContent = "Регистрация не пройдена";
+    registerForm.reportValidity();
+    return;
+  }
+
+  if (password.value !== confirmPassword.value) {
+    event.preventDefault();
+    message.textContent = "Пароли не совпадают";
+    return;
+  }
+
+  event.preventDefault(); // пока не отправляем форму на сервер
+
+  const data = Object.fromEntries(new FormData(registerForm));
+  const user = { ...data, createdOn: new Date () }
+  console.log(user);
+  
+  modal.classList.remove("showed")
+  overlay.classList.remove("open")
+});
+
+
+
+
